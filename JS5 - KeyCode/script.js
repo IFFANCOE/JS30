4ebeb11ec7f3f839aa1f03ffe60dc9ba1f4a5ee4
Iffan Pathan <vbaako1@hotmail.com>
2021-09-23 16:58:11 +0700

    KeyCode

---
+++ JS5 - KeyCode/script.js
@@ -0,0 +1,8 @@
+let info = document.getElementById('info'),
+    key = document.getElementById('key');
+document.onkeydown = document.body.onkeydown = function (e) {
+    e = e || window.event;
+    info.innerHTML= '';
+    key.innerHTML = e.keyCode || e.which;
+    return false;
+}
